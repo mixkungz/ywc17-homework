edf44dd7b2b4a1712ae0ed788e152e95e447a3d7
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-10-29 16:18:18 +0700

    :white_check_mark: :art: Use nth-child instead of first-child and last-child

--- cypress/integration/navbar.spec.js
+++ cypress/integration/navbar.spec.js
@@ -5,13 +5,13 @@ describe('Navbar Test', function() {
   })
   it('Each menu should has correct label and link', function() {
     cy.visit('http://localhost:8000/')
-    cy.get('[data-cy=menu] > li:first-child > a').should('contain', 'ลงทะเบียนเข้าร่วมมาตรการ')
-    cy.get('[data-cy=menu] > li:first-child > a').should('have.attr', 'href', 'https://regist.ชิมช้อปใช้.com/Register/')
+    cy.get('[data-cy=menu] > :nth-child(1)').should('contain', 'ลงทะเบียนเข้าร่วมมาตรการ')
+    cy.get('[data-cy=menu] > :nth-child(1)').should('have.attr', 'href', 'https://regist.ชิมช้อปใช้.com/Register/')
 
-    cy.get('[data-cy=menu] > :nth-child(2) > a').should('contain', 'ขั้นตอนการเข้าร่วม')
-    cy.get('[data-cy=menu] > :nth-child(2) > a').should('have.attr', 'href', 'https://www2.ชิมช้อปใช้.com/howto-register')
+    cy.get('[data-cy=menu] > :nth-child(2)').should('contain', 'ขั้นตอนการเข้าร่วม')
+    cy.get('[data-cy=menu] > :nth-child(2)').should('have.attr', 'href', 'https://www2.ชิมช้อปใช้.com/howto-register')
 
-    cy.get('[data-cy=menu] > li:last-child > a').should('contain', 'ร้านค้าที่เข้าร่วม')
-    cy.get('[data-cy=menu] > li:last-child > a').should('have.attr', 'href', 'https://www2.ชิมช้อปใช้.com/thung-ngern-shop-province')
+    cy.get('[data-cy=menu] > :nth-child(3)').should('contain', 'ร้านค้าที่เข้าร่วม')
+    cy.get('[data-cy=menu] > :nth-child(3)').should('have.attr', 'href', 'https://www2.ชิมช้อปใช้.com/thung-ngern-shop-province')
   })
 })
