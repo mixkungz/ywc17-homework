0759b81594ef78158e70d467fd87071b8e9a1967
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-10-30 16:02:56 +0700

    :lipstick: Changing text alignment on mobile

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 
 const WebNavbar = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home" className='d-lg-none'>React-Bootstrap</Navbar.Brand>
+  <Navbar bg="light" expand="md" fixed='top'>
+    <Navbar.Brand href="#home" className='d-md-none'>React-Bootstrap</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mx-lg-auto" data-cy='menu'>
+      <Nav className="text-right text-md-center mx-md-auto" data-cy='menu'>
         <Nav.Link href="https://regist.ชิมช้อปใช้.com/Register/">ลงทะเบียนเข้าร่วมมาตรการ</Nav.Link>
         <Nav.Link href="https://www2.ชิมช้อปใช้.com/howto-register">ขั้นตอนการเข้าร่วม</Nav.Link>
         <Nav.Link href="https://www2.ชิมช้อปใช้.com/thung-ngern-shop-province">ร้านค้าที่เข้าร่วม</Nav.Link>
