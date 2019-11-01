f0aa1f6cfa88a312eb413d1d86e6399344d695c4
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-11-01 16:01:10 +0700

    :bug: Fix not found selector

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,15 +1,15 @@
 describe('Footer Test', function() {
   it('Registration personal group information should has telephone link to ktb callcenter', function() {
     cy.visit('http://localhost:8000/')
-    cy.get('[data-cy=ktb-phonenumber]').should('have.attr', 'href', 'tel:021111144')
+    cy.get(`[data-cy=footer] [data-cy=ktb-phonenumber]`).should('have.attr', 'href', 'tel:021111144')
   })
   it('Registration corperate group information should has telephone link to CGD callcenter', function() {
     cy.visit('http://localhost:8000/')
-    cy.get('[data-cy=cgd-phonenumber]').should('have.attr', 'href', 'tel:022706400')
+    cy.get(`[data-cy=footer] [data-cy=cgd-phonenumber]`).should('have.attr', 'href', 'tel:022706400')
   })
   it('More information about travel campaign should has telephone link to TAT callcenter', function() {
     cy.visit('http://localhost:8000/')
-    cy.get('[data-cy=tat-phonenumber]').should('have.attr', 'href', 'tel:1672')
+    cy.get(`[data-cy=footer] [data-cy=tat-phonenumber]`).should('have.attr', 'href', 'tel:1672')
   })
   
 })
